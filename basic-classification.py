86695945a41a6c51b39d63f2636bb1eddc047de7
Wyatt Hoodes <hoodesw@hawaii.edu>
2019-01-03 09:09:21 -1000

    Finishing up

--- basic-classification.py
+++ basic-classification.py
@@ -28,7 +28,6 @@ train_images = train_images / 255.0
 test_images = test_images / 255.0
 
 # View the first 25 images, make sure the data is in the correct format
-
 plt.figure(figsize=(10,10))
 for i in range(25):
     plt.subplot(5, 5, i + 1)
@@ -62,3 +61,63 @@ print(predictions[0])
 print(np.argmax(predictions[0]))
 
 print(test_labels[0])
+
+# Graph setup
+
+def plot_image(i, predictions_array, true_label, img):
+  predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+
+  plt.imshow(img, cmap=plt.cm.binary)
+
+  predicted_label = np.argmax(predictions_array)
+  if predicted_label == true_label:
+    color = 'blue'
+  else:
+    color = 'red'
+
+  plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                100*np.max(predictions_array),
+                                class_names[true_label]),
+                                color=color)
+
+def plot_value_array(i, predictions_array, true_label):
+  predictions_array, true_label = predictions_array[i], true_label[i]
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+  thisplot = plt.bar(range(10), predictions_array, color="#777777")
+  plt.ylim([0, 1])
+  predicted_label = np.argmax(predictions_array)
+
+  thisplot[predicted_label].set_color('red')
+  thisplot[true_label].set_color('blue')
+
+# Plot the first X test images, their predicted label, and the true label
+# Color correct predictions in blue, incorrect predictions in red
+
+num_rows = 5
+num_cols = 3
+num_images = num_rows*num_cols
+plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+for i in range(num_images):
+  plt.subplot(num_rows, 2*num_cols, 2*i+1)
+  plot_image(i, predictions, test_labels, test_images)
+  plt.subplot(num_rows, 2*num_cols, 2*i+2)
+  plot_value_array(i, predictions, test_labels)
+
+# Make a prediction about a single image
+
+img = test_images[0]
+img = (np.expand_dims(img,0))
+
+# Now predict the image
+
+predictions_single = model.predict(img)
+print(predictions_single)
+
+plot_value_array(0, predictions_single, test_labels)
+_ = plt.xticks(range(10), class_names, rotation=45)
+plt.show()
